a3504580e7da2781b4b14541d18d919ca05ca551
Ke Yang <keyang@Kes-MacBook-Pro.local>
2019-12-10 10:11:24 -0600

    init

---
+++ precache-manifest.dec1bed9e2c075af6a6879cda91b4908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e77a3109f91f8c7baa218cc3508da28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2612b73381d4f9fe2c4",
+    "url": "/static/css/2.d741b2c7.chunk.css"
+  },
+  {
+    "revision": "ececaee706895f804542",
+    "url": "/static/css/main.e3c7e5e0.chunk.css"
+  },
+  {
+    "revision": "c2612b73381d4f9fe2c4",
+    "url": "/static/js/2.2442145d.chunk.js"
+  },
+  {
+    "revision": "ececaee706895f804542",
+    "url": "/static/js/main.511c3d19.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
